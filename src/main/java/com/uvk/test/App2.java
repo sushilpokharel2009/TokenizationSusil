ac11195919736c885e1cd8b74cbfa88ed723da5f
Shubhashree Pokharel <Shubhashree11@shubhashreesmbp.home>
2018-06-20 23:01:15 -0500

    first commit

---
+++ src/main/java/com/uvk/test/App2.java
@@ -0,0 +1,36 @@
+package com.uvk.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class App2 {
+	
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost:3306/vendorDB";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "";
+
+	   public static void main(String[] args) {
+		  
+		   String accountNumber = "accountNumber";
+		   String tableName = "VENDOR";
+		   
+//		   tokenizationcode
+		  
+		   
+		   
+		   
+		}//end main
+	   
+	   
+	   
+	   
+}
